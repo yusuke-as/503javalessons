a9ecd9d17e06371bb72061b0c39bc17e9dcf1cdc
yusuke <test@test>
2019-07-31 09:37:34 +0900

    rpg2フォルダ作成、継承用にhero,matango作成

---
+++ rpg2/Hero.java
@@ -0,0 +1,12 @@
+public class Hero{
+	private String name="ミナト";
+	private int hp=100;
+	public void attack(Matango m){
+		System.out.println(this.name+"の攻撃！");
+		m.hp -=5;
+		System.out.println("5ポイントのダメージを与えた！");
+	}
+	public void run(){
+		System.out.println(this.name+"は逃げ出した！");
+	}
+}
---
+++ rpg2/SuperHero.java
@@ -0,0 +1,14 @@
+public class SuperHero extends Hero{
+	private boolean flying;
+	public void fly(){
+		this.flying=true;
+		System.out.println("飛び上がった！");
+	}
+	public void land(){
+		this.flying=false;
+		System.out.println("着地した！");
+	}
+	public void run(){
+		System.out.println("撤退した。");
+	}
+}
